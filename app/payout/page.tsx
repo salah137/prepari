7a368d9acfdb1a25e743d3b08a2d868103590c1e
salah137 <boksaloh.com>
2024-06-27 00:53:31 +0100

    finished

--- app/payout/page.tsx
+++ app/payout/page.tsx
@@ -8,7 +8,7 @@ import { collection, doc, getDoc, getFirestore, setDoc, deleteDoc } from "fireba
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import logo from "../assets/logo.png"
-
+import wafa from "../assets/wafa.png"
 export default function page() {
     "use client"
 
@@ -29,54 +29,59 @@ export default function page() {
             })()
         }, []
     )
-    return  <div className="all">
-    <header className="main-header">
-        <Image src={logo} width={200} height={200} alt="logo" />
-    </header><main className="payout">
-        <div className="cih">
-            <Image src={cih} alt="cih" width={300} height={150} />
-            <h1>use CIH to pay</h1>
-        </div>
-        <div className="rib">
-            <div className="y-rib">
-                <h2> Give us your rib</h2>
-                <input type="text" value={rib} onChange={
-                    (e) => {
-                        setRib(e.target.value)
-                    }
-                }></input>
-            </div>
-
-            <div className="txt">
-                send money to this rib
-                <span className="m-rib">
-                    5810245211017400
-                </span>
-                and connect me at 
-                <span className="m-rib">
-                    +212 614107997
-                </span>
-                and now your account wait 24h to be verified
+    return <div className="all">
+        <header className="main-header">
+            <Image src={logo} width={200} height={200} alt="logo" />
+        </header><main className="payout">
+            <div className="cih">
+                <Image src={cih} alt="cih" width={300} height={150} />
+                <Image src={wafa} alt="cih" width={300} height={150} />
                 
             </div>
-            <button onClick={
-                async () => {
-                    if (rib) {
-
-                        let dd = {
-                            payed: false,
-                            uuid: user.uuid,
-                            rib: rib
+            <div className="rib">
+                <div className="y-rib">
+                    <h2> Give us your rib</h2>
+                    <input type="text" value={rib} onChange={
+                        (e) => {
+                            setRib(e.target.value)
                         }
-                        await deleteDoc(doc(db, "users", `${ref}`))
-                        let result = await setDoc(doc(db, "users", `${ref}`), dd, {
-                            merge: true,
-                        });
-                        router.push("/main")
-                    }
-                }
-            }>Done</button>
+                    }></input>
+                </div>
 
-        </div>
-    </main></div>
+                <div className="txt">
+                    send money to this rib
+                    <span className="m-rib">
+                        5810245211017400
+                    </span>
+                    and contact me at
+                    <span className="m-rib">
+                        +212 614107997
+                    </span>
+                    and now your account wait 24h to be verified
+                    Or use <span className="m-rib"> 
+                        wafa cash
+                    </span> and contact <span className="m-rib">
+                        +212 614107997
+                    </span>
+                </div>
+                <button onClick={
+                    async () => {
+                        if (rib) {
+
+                            let dd = {
+                                payed: false,
+                                uuid: user.uuid,
+                                rib: rib
+                            }
+                            await deleteDoc(doc(db, "users", `${ref}`))
+                            let result = await setDoc(doc(db, "users", `${ref}`), dd, {
+                                merge: true,
+                            });
+                            router.push("/main")
+                        }
+                    }
+                }>Done</button>
+
+            </div>
+        </main></div>
 }
